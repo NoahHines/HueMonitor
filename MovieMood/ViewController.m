9c41115096539f24ba41236b78fb0589335cf913
mdlinington <mdlinington@gmail.com>
2015-01-17 02:08:36 -0500

    Continuously scans for colors underneath pixel

--- MovieMood/ViewController.m
+++ MovieMood/ViewController.m
@@ -13,20 +13,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSPoint origin = NSMakePoint(100, 100);
-    //[self.focusView lockFocus];
-    //NSColor *topColor = NSReadPixel(origin);
-    //[self.view unlockFocus];
     
-    CGDirectDisplayID ids[10];
-    CGGetDisplaysWithPoint(origin, 10, ids, nil);
-    CGImageRef image = CGDisplayCreateImageForRect(ids[0], CGRectMake(0, 0, 1, 1));
-    NSBitmapImageRep *bitmap = [[NSBitmapImageRep alloc] initWithCGImage:image];
-    NSColor *color = [bitmap colorAtX:0 y:0];
-    
+    NSColor *color = [self getColorFromMouseCursor];
     NSLog(@"%f, %f, %f",color.redComponent,color.greenComponent,color.blueComponent);
+    
+    dispatch_queue_t colorQueue = dispatch_queue_create("colorQueue", DISPATCH_QUEUE_SERIAL);
+
+    dispatch_async(colorQueue, ^{
+        [self constantlyGetColor];
+    });
 
     // Do any additional setup after loading the view.
+}
+
+- (void) constantlyGetColor
+{
+    while (1) {
+        NSColor *color = [self getColorFromMouseCursor];
+        NSLog(@"%f, %f, %f",color.redComponent,color.greenComponent,color.blueComponent);
+    }
+}
+
+- (NSColor *) getColorFromMouseCursor {
+    CGPoint mousePoint = NSPointToCGPoint([NSEvent mouseLocation]);
+    mousePoint.y = [NSScreen mainScreen].frame.size.height - mousePoint.y;
+    
+    CGDirectDisplayID ids;
+    CGGetDisplaysWithPoint(mousePoint, 1, &ids, nil);
+    CGImageRef image = CGDisplayCreateImageForRect(ids, CGRectMake(mousePoint.x, mousePoint.y, 1, 1));
+    NSBitmapImageRep *bitmap = [[NSBitmapImageRep alloc] initWithCGImage:image];
+    CGImageRelease(image);
+    NSColor *color = [bitmap colorAtX:0 y:0];
+    return color;
 }
 
 - (void)setRepresentedObject:(id)representedObject {
