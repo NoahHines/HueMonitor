9b870a7cabe0ab23681b76213583acfc9e07e83a
mdlinington <mdlinington@gmail.com>
2015-01-18 10:46:22 -0500

    Switched timers to a timer that the OS doesn't sleep in background

--- HueMonitor/MMWatchPixelController.m
+++ HueMonitor/MMWatchPixelController.m
@@ -13,7 +13,7 @@
 @interface MMWatchPixelController ()
 
 @property (strong, atomic) NSMutableArray *pixelArray;
-@property (strong, atomic) NSTimer *monitorTimer;
+@property (strong, atomic) dispatch_source_t monitorTimer;
 
 @end
 
@@ -52,6 +52,14 @@
 // This gets called repeatedly by an NSTimer when monitoring
 - (void) repeatPixels:(NSTimer *) timer
 {
+//    NSLog(@"Hit");
+//    CGEventSourceRef source = CGEventSourceCreate(kCGEventSourceStateCombinedSessionState);
+//    CGPoint newLocation = CGPointMake([NSEvent mouseLocation].x, [NSScreen mainScreen].frame.size.height - [NSEvent mouseLocation].y+1);
+//    CGEventRef mouseMove = CGEventCreateMouseEvent(source, kCGEventMouseMoved, newLocation, 0);
+//    CGEventPost(kCGHIDEventTap, mouseMove);
+//    CFRelease(mouseMove);
+//    CFRelease(source);
+
     for(int i = 0; i < self.lightArray.count; i++)
     {
         NSArray *lightHotspots = [self.pixelArray subarrayWithRange:NSMakeRange(i*self.pixelsPerLight, self.pixelsPerLight)];
@@ -90,13 +98,24 @@
 
 - (void) startMonitoring
 {
-    self.monitorTimer = [NSTimer scheduledTimerWithTimeInterval:0.25 target:self selector:@selector(repeatPixels:) userInfo:nil repeats:YES];
-    [[NSRunLoop currentRunLoop] addTimer:self.monitorTimer forMode:NSDefaultRunLoopMode];
+    if(!self.monitorTimer)
+    {
+        self.monitorTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, DISPATCH_TIMER_STRICT, dispatch_get_main_queue());
+        dispatch_source_set_event_handler(self.monitorTimer, ^{
+            [self repeatPixels:nil];
+        });
+        dispatch_source_set_timer(self.monitorTimer, dispatch_time(DISPATCH_TIME_NOW, 0.25 * NSEC_PER_SEC), 0.25 * NSEC_PER_SEC, 0.125 * NSEC_PER_SEC);
+    }
+    dispatch_resume(self.monitorTimer);
+    
+    
+//    self.monitorTimer = [NSTimer scheduledTimerWithTimeInterval:0.25 target:self selector:@selector(repeatPixels:) userInfo:nil repeats:YES];
+//    [[NSRunLoop currentRunLoop] addTimer:self.monitorTimer forMode:NSDefaultRunLoopMode];
 }
 
 - (void) stopMonitoring
 {
-    [self.monitorTimer invalidate];
+    dispatch_suspend(self.monitorTimer);
 }
 
 
